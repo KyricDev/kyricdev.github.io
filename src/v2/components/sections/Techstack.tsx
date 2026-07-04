fd32aaca7e2cf0ee6d1676f3b7388ede59c6ac07
Ricky Dave Madrid <madridrickydave@gmail.com>
2026-07-05 01:28:28 +0800

    feat: Techstack section

--- src/v2/components/sections/Techstack.tsx
+++ src/v2/components/sections/Techstack.tsx
@@ -1,4 +1,6 @@
+import { Divider } from "@/v2/components/ui/Divider";
 import { getBackendIconMap, getFrontendIconMap } from "@/v2/lib/iconHelpers";
+import type { IconType } from "react-icons";
 
 export function Techstack() {
   const frontendIcons = getFrontendIconMap();
@@ -7,35 +9,51 @@ export function Techstack() {
   return (
     <div
       id="techstack"
-      className="flex font-thin text-white pl-100 py-25 gap-20 h-screen justify-center"
+      className="flex font-thin text-white pl-80 py-25 gap-20 h-screen justify-center"
     >
       <div className="flex flex-col text-right gap-10">
-        <div className="text-7xl">Frontend</div>
+        <div className="text-6xl">Frontend</div>
         <div className="gap-5 flex flex-col items-end">
           {Object.values(frontendIcons).map(({ icon: Icon, label }, index) => {
             return (
-              <div className="flex items-center gap-5" key={index}>
-                <div className="text-5xl">{label}</div>
-                <Icon className="text-6xl" />
-              </div>
+              <TechComponent Icon={Icon} label={label} reverse key={index} />
             );
           })}
         </div>
       </div>
-      <div className="bg-primary items-stretch w-px" />
+      <Divider orientation="vertical" />
       <div className="flex gap-10 flex-col">
-        <div className="text-7xl">Backend</div>
+        <div className="text-6xl">Backend</div>
         <div className="gap-5 flex flex-col">
           {Object.values(backendIcons).map(({ icon: Icon, label }, index) => {
-            return (
-              <div className="flex items-center gap-5" key={index}>
-                <Icon className="text-6xl" />
-                <div className="text-5xl">{label}</div>
-              </div>
-            );
+            return <TechComponent Icon={Icon} label={label} key={index} />;
           })}
         </div>
       </div>
+    </div>
+  );
+}
+
+type TechComponentProps = {
+  reverse?: boolean;
+  label: string;
+  Icon: IconType;
+};
+
+function TechComponent({ Icon, label, reverse }: TechComponentProps) {
+  if (reverse) {
+    return (
+      <div className="flex items-center gap-5">
+        <div className="text-4xl">{label}</div>
+        <Icon className="text-3xl" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex items-center gap-5">
+      <Icon className="text-4xl" />
+      <div className="text-3xl">{label}</div>
     </div>
   );
 }
