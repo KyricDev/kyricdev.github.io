42e534d47987dcc3f41083a18595a56c863ec457
Ricky Dave Madrid <madridrickydave@gmail.com>
2026-07-06 14:59:01 +0800

    feat: improved BlurredMovingCircle size calculations

--- src/v2/components/ui/BlurredMovingCircle.tsx
+++ src/v2/components/ui/BlurredMovingCircle.tsx
@@ -1,14 +1,35 @@
 import { useRandomPosition } from "@/v2/hooks/useRandomPosition";
+import { useEffect, useRef, useState } from "react";
 
 type BlurredMovingCircleProps = {
   size?: number;
 };
 
-export function BlurredMovingCircle({ size }: BlurredMovingCircleProps) {
-  const { pos1, pos2 } = useRandomPosition(5000);
+export function BlurredMovingCircle({
+  size: initialSize,
+}: BlurredMovingCircleProps) {
+  const { pos1, pos2 } = useRandomPosition({
+    interval: 5000,
+    usePercentage: false,
+  });
+  const [parentHeight, setParentHeight] = useState(0);
+  const [parentWidth, setParentWidth] = useState(0);
+  const [size, setSize] = useState(initialSize ?? 600);
+  const element = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const parent = element.current?.parentElement;
+    if (parent) {
+      const { clientHeight, clientWidth } = parent;
+      setParentHeight(clientHeight);
+      setParentWidth(clientWidth);
+      setSize(Math.min(clientHeight, clientWidth) / 2);
+    }
+  }, [element]);
 
   return (
     <div
+      ref={element}
       className="
           absolute
           rounded-full
@@ -16,13 +37,13 @@ export function BlurredMovingCircle({ size }: BlurredMovingCircleProps) {
           -z-10
           top-0
           left-0
-          blur-[600px]
-          transition-all duration-5000
+          transition-transform duration-5000
         "
       style={{
-        transform: `translate(${pos1 * 2}%, ${pos2}%)`,
         height: size ?? 600,
         width: size ?? 600,
+        transform: `translate(${pos2 * parentWidth}px, ${pos1 * parentHeight}px)`,
+        filter: `blur(${size}px)`,
       }}
     />
   );
